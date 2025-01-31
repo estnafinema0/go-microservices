bf1f8add0bc0ff64d8687b81ebc1d78aeb939b40
estnafinema0 <Kat9765830@gmail.com>
2025-02-01 00:59:10 +0300

    GorillaBranch: Remove ServeHTTP method and simplify Product handler routing

--- handlers/product.go
+++ handlers/product.go
@@ -3,8 +3,6 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
 
 	"github.com/estnafinema0/go-microservices/data"
 )
@@ -15,48 +13,6 @@ type Product struct {
 
 func NewProduct(l *log.Logger) *Product {
 	return &Product{l}
-}
-
-func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.GetProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.AddProduct(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		// Expect the id in the URI
-		re := regexp.MustCompile(`/([0-9]+)`)
-		matches := re.FindAllStringSubmatch(r.URL.Path, -1)
-		if len(matches) != 1 {
-			p.l.Println("Invalid URI: more than one id")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-		if len(matches[0]) != 2 {
-			p.l.Println("Invalid URI: more than one capture group")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := matches[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			p.l.Println("Invalid URI: unable to convert to number")
-			http.Error(rw, "Invalid ID", http.StatusBadRequest)
-			return
-		}
-		p.l.Println("Got id:", id)
-		p.UpdateProduct(id, rw, r)
-		return
-	}
-
-	// Catch all
-	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Product) GetProducts(rw http.ResponseWriter, r *http.Request) {
