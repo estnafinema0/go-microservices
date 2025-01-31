f39db314a925ee13019a27831ce0790c1b54a31e
estnafinema0 <Kat9765830@gmail.com>
2025-01-31 21:29:18 +0300

    Implement GET products endpoint with JSON serialization

--- handlers/product.go
+++ handlers/product.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/estnafinema0/go-microservices/data"
 )
 
 type Product struct {
@@ -14,5 +17,10 @@ func NewProduct(l *log.Logger) *Product {
 }
 
 func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-
+	lp := data.GetProducts()
+	d, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+	rw.Write(d)
 }
