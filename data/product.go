2eb2da94ad6d584a27c2e9f5bda9e629f50643ff
estnafinema0 <Kat9765830@gmail.com>
2025-01-31 21:22:03 +0300

    Add Product struct and initial product list in data package

---
+++ data/product.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frosty milky coffee",
+		Price:       300.0,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Strong coffee without milk",
+		Price:       180.0,
+		SKU:         "abc124",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
