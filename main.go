0eda6fe205d332886ce44c62aff90eea536ac64f
estnafinema0 <Kat9765830@gmail.com>
2025-02-01 18:57:29 +0300

    Refactor server configuration and improve error handling in main.go

--- main.go
+++ main.go
@@ -31,11 +31,15 @@ import (
 var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind Address for the server")
 
 func main() {
+
 	env.Parse()
 
-	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
 	ph := handlers.NewProducts(l)
 
+	// create a new serve mux and register the handlers
 	sm := mux.NewRouter()
 
 	getRouter := sm.Methods(http.MethodGet).Subrouter()
@@ -43,26 +47,30 @@ func main() {
 
 	putRouter := sm.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
-	putRouter.Use(ph.MiddlewareProductValidation)
+	putRouter.Use(ph.MiddlewareValidateProduct)
 
 	postRouter := sm.Methods(http.MethodPost).Subrouter()
 	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
-	s := &http.Server{
-		Addr:         *bindAddress,
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
 		l.Println("Starting server on port 9090")
 
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Printf("Error starting server: %s\n", err)
-			l.Fatal(err)
+			os.Exit(1)
 		}
 	}()
 
@@ -72,7 +80,7 @@ func main() {
 	sig := <-sigChan
 	l.Println("Received terminate, graceful shutdown", sig)
 
-	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	s.Shutdown(tc)
+	s.Shutdown(ctx)
 }
